cf6ecdf5a0c4190bfbb500fa538c9970e9e6751c
Roy Shilkrot <roy.shil@gmail.com>
2023-07-31 11:09:46 +0300

    url direct

--- cmake/BuildPugiXML.cmake
+++ cmake/BuildPugiXML.cmake
@@ -2,9 +2,8 @@ include(ExternalProject)
 
 ExternalProject_Add(
   pugixml_build
-  GIT_REPOSITORY git@github.com:zeux/pugixml.git
-  GIT_TAG v1.13
-  GIT_SHALLOW TRUE
+  URL https://github.com/zeux/pugixml/releases/download/v1.13/pugixml-1.13.tar.gz
+  URL_MD5 3e4c588e03bdca140844f3c47c1a995e
   CMAKE_GENERATOR ${CMAKE_GENERATOR}
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_SHARED_LIBS=OFF -DPUGIXML_BUILD_TESTS=OFF)
 
@@ -19,11 +18,9 @@ set(pugixml_lib_location ${INSTALL_DIR}/lib/${pugixml_lib_filename})
 message(STATUS "pugixml library expected at ${pugixml_lib_location}")
 
 add_library(pugixml STATIC IMPORTED)
-set_target_properties(
-  pugixml
-  PROPERTIES IMPORTED_LOCATION ${pugixml_lib_location})
+set_target_properties(pugixml PROPERTIES IMPORTED_LOCATION ${pugixml_lib_location})
 
 add_library(libpugixml INTERFACE)
 add_dependencies(libpugixml pugixml_build)
 target_link_libraries(libpugixml INTERFACE pugixml)
-set_target_properties(libpugixml PROPERTIES INTERFACE_INCLUDE_DIRECTORIES  ${INSTALL_DIR}/include)
+set_target_properties(libpugixml PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
