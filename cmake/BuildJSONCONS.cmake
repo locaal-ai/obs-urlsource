d5fec65c123ce637ebf93b95ecbbdc548149e694
Roy Shilkrot <roy.shil@gmail.com>
2023-10-03 13:32:43 -0400

    cmake

---
+++ cmake/BuildJSONCONS.cmake
@@ -0,0 +1,4 @@
+
+set(JSONCONS_BUILD_TESTS OFF)
+add_subdirectory(vendor/jsoncons EXCLUDE_FROM_ALL)
+
--- src/parsers/errors.cpp
+++ src/parsers/errors.cpp
@@ -5,7 +5,7 @@
 
 struct request_data_handler_response make_fail_parse_response(const std::string &error_message)
 {
-	obs_log(LOG_INFO, "Failed to parse response: %s", error_message);
+	obs_log(LOG_INFO, "Failed to parse response: %s", error_message.c_str());
 	// Build an error response
 	struct request_data_handler_response responseFail;
 	responseFail.error_message = error_message;
