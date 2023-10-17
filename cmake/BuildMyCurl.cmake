24bbee0408141a18a3f560ed6b4bcfd2ae8f0061
Roy Shilkrot <roy.shil@gmail.com>
2023-10-17 10:55:29 -0400

    fix link

--- cmake/BuildMyCurl.cmake
+++ cmake/BuildMyCurl.cmake
@@ -1,38 +1,38 @@
 include(FetchContent)
 
-set(LibCurl_VERSION "8.4.0")
+set(LibCurl_VERSION "8.4.0-1")
 set(LibCurl_BASEURL "https://github.com/obs-ai/obs-ai-libcurl-dep/releases/download/${LibCurl_VERSION}")
 
 if(${CMAKE_BUILD_TYPE} STREQUAL Release OR ${CMAKE_BUILD_TYPE} STREQUAL RelWithDebInfo)
-set(LibCurl_BUILD_TYPE Release)
+  set(LibCurl_BUILD_TYPE Release)
 else()
-set(LibCurl_BUILD_TYPE Debug)
+  set(LibCurl_BUILD_TYPE Debug)
 endif()
 
 if(APPLE)
-if(LibCurl_BUILD_TYPE STREQUAL Release)
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-macos-Release.tar.gz")
-    set(LibCurl_HASH MD5=30FBFA06FCE476B6A8520984703BC2B7)
-else()
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-macos-Debug.tar.gz")
-    set(LibCurl_HASH MD5=E13398CF463F13EDA946712F66883BD0)
-endif()
+  if(LibCurl_BUILD_TYPE STREQUAL Release)
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-macos-master-Release.tar.gz")
+    set(LibCurl_HASH MD5=C89D8BC38E221737B7D5C9E5AE18C079)
+  else()
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-macos-master-Debug.tar.gz")
+    set(LibCurl_HASH MD5=F2A643B44D8626119DDB326C5AFFD704)
+  endif()
 elseif(MSVC)
-if(LibCurl_BUILD_TYPE STREQUAL Release)
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-windows-Release.zip")
-    set(LibCurl_HASH MD5=FC0CC7E9730BDCD9A50312A7D176E963)
+  if(LibCurl_BUILD_TYPE STREQUAL Release)
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-windows-master-Release.zip")
+    set(LibCurl_HASH MD5=81BC6A5004F0D52AA082A25B813B1362)
+  else()
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-windows-master-Debug.zip")
+    set(LibCurl_HASH MD5=8DC73477E3D427E2EA5124B57985BF47)
+  endif()
 else()
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-windows-Debug.zip")
-    set(LibCurl_HASH MD5=09D73B647EE8CC68408F5276A0DB81EB)
-endif()
-else()
-if(LibCurl_BUILD_TYPE STREQUAL Release)
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-linux-Release.tar.gz")
-    set(LibCurl_HASH MD5=B058DB62F34DA44688DA829453B4C59D)
-else()
-    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-linux-Debug.tar.gz")
-    set(LibCurl_HASH MD5=ADF46200BC5DF9E883015B0EFE7F8CCB)
-endif()
+  if(LibCurl_BUILD_TYPE STREQUAL Release)
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-linux-master-Release.tar.gz")
+    set(LibCurl_HASH MD5=EA9537A0BDBA1C53DC4A54FE767E90E8)
+  else()
+    set(LibCurl_URL "${LibCurl_BASEURL}/libcurl-linux-master-Debug.tar.gz")
+    set(LibCurl_HASH MD5=6F405CFA0398A04ADD1C2FCE9596F8B8)
+  endif()
 endif()
 
 FetchContent_Declare(
@@ -41,40 +41,31 @@ FetchContent_Declare(
   URL_HASH ${LibCurl_HASH})
 FetchContent_MakeAvailable(libcurl_fetch)
 
-add_library(libcurl INTERFACE)
 if(MSVC)
-  target_link_libraries(
-    libcurl
-    INTERFACE ${libcurl_fetch_SOURCE_DIR}/lib/libcurl.lib)
-  target_include_directories(libcurl SYSTEM INTERFACE ${libcurl_fetch_SOURCE_DIR}/include)
+  set(libcurl_fetch_lib_location "${libcurl_fetch_SOURCE_DIR}/lib/libcurl.lib")
+  set(libcurl_fetch_link_libs "\$<LINK_ONLY:ws2_32>;\$<LINK_ONLY:advapi32>;\$<LINK_ONLY:crypt32>;\$<LINK_ONLY:bcrypt>")
 else()
-  target_link_libraries(
-    libcurl INTERFACE ${libcurl_fetch_SOURCE_DIR}/lib/libcurl.a)
-  target_include_directories(libcurl SYSTEM INTERFACE ${libcurl_fetch_SOURCE_DIR}/include)
+  set(libcurl_fetch_lib_location "${libcurl_fetch_SOURCE_DIR}/lib/libcurl.a")
+  if(UNIX AND NOT APPLE)
+    set(libcurl_fetch_link_libs "\$<LINK_ONLY:OpenSSL::SSL>;\$<LINK_ONLY:OpenSSL::Crypto>;\$<LINK_ONLY:ZLIB::ZLIB>")
+  else()
+    set(libcurl_fetch_link_libs
+        "-framework SystemConfiguration;-framework Security;-framework CoreFoundation -framework CoreServices;\$<LINK_ONLY:ZLIB::ZLIB>"
+    )
+  endif()
 endif()
 
+# Create imported target
+add_library(libcurl STATIC IMPORTED)
 
-# if(OS_MACOS)
-#   set(CURL_USE_OPENSSL OFF)
-#   set(CURL_USE_SECTRANSP ON)
-# elseif(OS_WINDOWS)
-#   set(CURL_USE_OPENSSL OFF)
-#   set(CURL_USE_SCHANNEL ON)
-# elseif(OS_LINUX)
-#   add_compile_options(-fPIC)
-#   set(CURL_USE_OPENSSL ON)
-# endif()
-# set(BUILD_CURL_EXE OFF)
-# set(BUILD_SHARED_LIBS OFF)
-# set(HTTP_ONLY ON)
-# set(CURL_USE_LIBSSH2 OFF)
-# set(BUILD_TESTING OFF)
-# set(PICKY_COMPILER OFF)
-
-# add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/curl EXCLUDE_FROM_ALL)
-# if(OS_MACOS)
-#   target_compile_options(
-#     libcurl PRIVATE -Wno-error=ambiguous-macro -Wno-error=deprecated-declarations -Wno-error=unreachable-code
-#                     -Wno-error=unused-parameter -Wno-error=unused-variable)
-# endif()
-# include_directories(SYSTEM ${CMAKE_SOURCE_DIR}/vendor/curl/include)
+set_target_properties(
+  libcurl
+  PROPERTIES INTERFACE_COMPILE_DEFINITIONS "CURL_STATICLIB"
+             INTERFACE_INCLUDE_DIRECTORIES "${libcurl_fetch_SOURCE_DIR}/include"
+             INTERFACE_LINK_LIBRARIES "${libcurl_fetch_link_libs}")
+set_property(
+  TARGET libcurl
+  APPEND
+  PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libcurl PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C" IMPORTED_LOCATION_RELEASE
+                                                                                       ${libcurl_fetch_lib_location})
