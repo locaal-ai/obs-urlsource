6175dd98e463551971c47649c002d8a0304e3801
Roy Shilkrot <roy.shil@gmail.com>
2023-11-02 01:56:31 -0400

    fix win32 cmake

--- cmake/BuildLexbor.cmake
+++ cmake/BuildLexbor.cmake
@@ -3,16 +3,30 @@ include(ExternalProject)
 if(APPLE)
   set(LEXBOR_CMAKE_PLATFORM_OPTIONS -DCMAKE_OSX_ARCHITECTURES=x86_64$<SEMICOLON>arm64)
 else()
-  set(LEXBOR_CMAKE_PLATFORM_OPTIONS "")
+  if(WIN32)
+    add_compile_definitions(LEXBOR_STATIC=1)
+    set(LEXBOR_CMAKE_PLATFORM_OPTIONS "-DCMAKE_C_FLAGS=/W3 /utf-8 /MP" "-DCMAKE_CXX_FLAGS=/W3 /utf-8 /MP")
+  else()
+    set(LEXBOR_CMAKE_PLATFORM_OPTIONS -DCMAKE_SYSTEM_NAME=Linux)
+  endif()
 endif()
+
+set(lexbor_lib_filename ${CMAKE_STATIC_LIBRARY_PREFIX}lexbor_static${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 ExternalProject_Add(
   lexbor_build
   GIT_REPOSITORY https://github.com/lexbor/lexbor.git
   GIT_TAG v2.3.0
   CMAKE_GENERATOR ${CMAKE_GENERATOR}
-  INSTALL_BYPRODUCTS <INSTALL_DIR>/lib/${CMAKE_STATIC_LIBRARY_PREFIX}lexbor${CMAKE_STATIC_LIBRARY_SUFFIX}
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DLEXBOR_BUILD_SHARED=OFF -DLEXBOR_BUILD_STATIC=ON
+  INSTALL_BYPRODUCTS <INSTALL_DIR>/lib/${lexbor_lib_filename}
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+             -DLEXBOR_BUILD_SHARED=OFF
+             -DLEXBOR_BUILD_STATIC=ON
+             -DLEXBOR_BUILD_TESTS_CPP=OFF
+             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+             -DCMAKE_LINKER=${CMAKE_LINKER}
              ${LEXBOR_CMAKE_PLATFORM_OPTIONS})
 
 ExternalProject_Get_Property(lexbor_build INSTALL_DIR)
@@ -20,7 +34,6 @@ ExternalProject_Get_Property(lexbor_build INSTALL_DIR)
 message(STATUS "lexbor will be installed to ${INSTALL_DIR}")
 
 # find the library
-set(lexbor_lib_filename ${CMAKE_STATIC_LIBRARY_PREFIX}lexbor_static${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(lexbor_lib_location ${INSTALL_DIR}/lib/${lexbor_lib_filename})
 
 message(STATUS "lexbor library expected at ${lexbor_lib_location}")
