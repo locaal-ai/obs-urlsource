b840b2dfef4e737e63d3d24bf47dd847a2687c1e
Roy Shilkrot <roy.shil@gmail.com>
2023-11-19 21:38:57 -0500

    more attempt

--- cmake/BuildPugiXML.cmake
+++ cmake/BuildPugiXML.cmake
@@ -16,15 +16,11 @@ ExternalProject_Add(
   INSTALL_BYPRODUCTS <INSTALL_DIR>/include
   BUILD_BYPRODUCTS <INSTALL_DIR>/lib/${pugixml_lib_filename}
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_SHARED_LIBS=OFF -DPUGIXML_BUILD_TESTS=OFF
-             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${PUGIXML_CMAKE_PLATFORM_OPTIONS})
+             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${PUGIXML_CMAKE_PLATFORM_OPTIONS} -DCMAKE_INSTALL_LIBDIR=lib)
 
 ExternalProject_Get_Property(pugixml_build INSTALL_DIR)
 
-# try to find for pugixml in ${INSTALL_DIR}/lib/ or  ${INSTALL_DIR}/lib64/
-find_library(
-  pugixml_lib_location
-  NAMES ${pugixml_lib_filename}
-  HINTS ${INSTALL_DIR}/lib ${INSTALL_DIR}/lib64)
+set(pugixml_lib_location ${INSTALL_DIR}/lib/${pugixml_lib_filename})
 
 add_library(pugixml_internal STATIC IMPORTED)
 add_dependencies(pugixml_internal pugixml_build)
