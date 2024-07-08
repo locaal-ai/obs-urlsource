a4d7250d9d729f88342334ca8f849fd2b29054f2
Roy Shilkrot <roy.shil@gmail.com>
2024-07-08 06:53:52 -0400

    chore: Add compile option to disable conversion warnings in JSONCONS build

--- cmake/BuildJSONCONS.cmake
+++ cmake/BuildJSONCONS.cmake
@@ -1,2 +1,3 @@
 set(JSONCONS_BUILD_TESTS OFF)
+add_compile_options(-Wno-conversion -Wno-error=conversion)
 add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/jsoncons ${CMAKE_BINARY_DIR}/jsoncons EXCLUDE_FROM_ALL)
